5618f059b4f3beabe57aec50082ac2f5f416e40b
EdwardW710 <EdwardW710@yahoo.com>
2025-02-14 09:06:43 -0500

    exercise 1 completed

--- server.js
+++ server.js
@@ -50,9 +50,14 @@ const RESTAURANT = {
     ]
   }
   
+// exercise 1
 
 app.get('/', (req, res) => {
   res.render('home.ejs', { restaurant: RESTAURANT });
+});
+
+app.get('/menu', (req, res) => {
+    res.render('menu.ejs', {menu: RESTAURANT.menu});
 });
 
 app.listen(3000, () => {
