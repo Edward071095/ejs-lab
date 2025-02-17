aef798d3648635a320d12fa1321972a54fbcedd6
EdwardW710 <EdwardW710@yahoo.com>
2025-02-17 13:57:08 -0500

    exercise 3 completed finished lab

--- server.js
+++ server.js
@@ -50,17 +50,74 @@ const RESTAURANT = {
     ]
   }
   
-// exercise 1
+// exercise one
 
 app.get('/', (req, res) => {
   res.render('home.ejs', { restaurant: RESTAURANT });
 });
 
+//exercise two
+
 app.get('/menu', (req, res) => {
     res.render('menu.ejs', {menu: RESTAURANT.menu});
 });
 
-//exercise two
+//exercise three
+
+// app.get('/menu/:category', (req, res) => {
+//   res.render('category.ejs', {menu: RESTAURANT.menu.category});
+// });
+
+const menuItems = [
+  { 
+    id: 1,
+    name: 'Quantum Quinoa Mushroom Burger',
+    price: 13.00,
+    rating: 4,
+    category: 'mains',
+    details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+  },
+  { 
+    id: 2,
+    name: 'Binary Berry Cheesecake',
+    price: 10.11,
+    rating: 3,
+    category: 'desserts',
+    details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+  },
+  { 
+    id: 3,
+    name: 'Recursive Rigatoni',
+    price: 17.00,
+    rating: 5,
+    category: 'mains',
+    details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
+  },
+  { 
+    id: 4,
+    name: 'Pumpkin Pi Squared',
+    price: 3.14,
+    rating: 5,
+    category: 'desserts',
+    details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+  },
+  { 
+    id: 5,
+    name: 'Fibonacci String Bean Fries',
+    price: 11.23,
+    rating: 5,
+    category: 'sides',
+    details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
+  }
+];
+
+
+
+app.get('/menu/:category', (req, res) => {
+  const menuCategorys = menuItems.filter(item => item.category === req.params.category)
+
+  res.render('category.ejs', { menuItems: menuCategorys });
+});
 
 
 
